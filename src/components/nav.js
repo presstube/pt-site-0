31d5d5aa6749c06f977eeba4e227f9130233a825
James Paterson <j@presstube.com>
2016-06-07 12:42:24 -0400

    some routes and logo sub-pixel finesse

--- src/components/nav.js
+++ src/components/nav.js
@@ -2,16 +2,24 @@
 import React from "react"
 
 import Brand from "./brand"
+import NavLink from "./nav-link"
 
 export default class Nav extends React.Component {
   render () {
     return (
       <div
         style={{
-          margin: 10
+          margin: 20
         }}
       >
-        <Brand name="brand" />
+        <Brand name="brand"/>
+        <ul>
+          <li><NavLink to="/sketchbook">Sketchbook</NavLink></li>
+          <li><NavLink to="/wall">Wall</NavLink></li>
+          <li><NavLink to="/boiled-drawings">Boiled Drawings</NavLink></li>
+          <li><NavLink to="/mala">Mala</NavLink></li>
+        </ul>
+        <Brand name="brand"/>
       </div>
     )
   }
