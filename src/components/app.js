cd3ef1e85c682cd2415dd1446b042239fdf85a39
James Paterson <j@presstube.com>
2016-06-07 13:11:30 -0400

    adding mobile meta

--- src/components/app.js
+++ src/components/app.js
@@ -6,7 +6,12 @@ import Nav from "./nav"
 export default class App extends React.Component {
   render () {
     return (
-      <div>
+      <div
+        style={{
+          // width: window.innerWidth,
+          // height: window.innerHeight,
+          // backgroundColor: "pink"
+        }}>
         <Nav />
         {this.props.children}
       </div>
