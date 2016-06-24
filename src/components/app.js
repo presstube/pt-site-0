1f314a320441eda5410213a69abd56a62ddd7a5d
James Paterson <j@presstube.com>
2016-06-24 05:45:16 -0400

    project unit detail links below unit title

--- src/components/app.js
+++ src/components/app.js
@@ -10,7 +10,7 @@ export default class App extends React.Component {
         // width: "100%",
         width: "calc(100% - 40px)",
         // width: sw - (spacing*2),
-        // maxWidth: 1400
+        maxWidth: 1400
       }}>
         <div // nav
           style={{
@@ -36,18 +36,20 @@ export default class App extends React.Component {
             }}
             src="https://player.vimeo.com/video/172081140"
             width="100%"
-            // height="360"
+            height="100%"
             frameborder="0"
             webkitallowfullscreen
             mozallowfullscreen
             allowfullscreen
           ></iframe>
           <p>
-            <a href="">Intro</a>,
-            <a href="">1-Life</a>,
-            <a href="">2-Imagination</a>,
-            <a href="">3-Meditation</a>,
-            <a href="">4-Systems</a>
+            <h3>
+              <a href="">Intro</a>,
+              <a href="">1-Life</a>,
+              <a href="">2-Imagination</a>,
+              <a href="">3-Meditation</a>,
+              <a href="">4-Systems</a>
+            </h3>
           </p>
         </div>
         <div // project unit
@@ -58,13 +60,15 @@ export default class App extends React.Component {
             src="img/dark-lord-grocery-delivery.jpg"
             style={{width: "100%"}}
           />
-          <p>
-            <a href="">full</a>,
-            <a href="">detail 1</a>,
-            <a href="">detail 2</a>,
-            <a href="">detail 3</a>
-          </p>
           <h2>Dark Lord Grocery Delivery</h2>
+          <p>
+            <h3>
+              <a href="">full</a>,
+              <a href="">detail 1</a>,
+              <a href="">detail 2</a>,
+              <a href="">detail 3</a>
+            </h3>
+          </p>
           <p>An 8x10.6" original sketchbook drawing from the Psychic Vomit Portrait series.</p>
           <p>Not all dark lords have glamorous jobs like stealing souls and ruling galaxies.</p>
           <p>Some deliver dark groceries.</p>
