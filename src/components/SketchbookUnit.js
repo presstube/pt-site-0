9e7f8dbf3d4023984750327515b41e7b0817c375
James Paterson <j@presstube.com>
2016-06-28 15:24:16 -0400

    cleaned up comments

--- src/components/SketchbookUnit.js
+++ src/components/SketchbookUnit.js
@@ -1,81 +1,14 @@
 import _ from "lodash"
 import React from "react"
-import C from "../constants"
 
 export default class SketchbookUnit extends React.Component {
-
-  constructor(props) {
-    super(props)
-    this.initWinHeight = window.innerHeight
-  }
-
-  // state = {
-  //   active: false,
-  //   rect: {top: 0, right: 0, bottom: 0, width: 0},
-  //   screenWidth: window.innerWidth,
-  //   screenHeight: window.innerHeight,
-  // }
-
-  componentDidMount() {
-    // this.calcResize()
-    // this.checkActive()
-    // window.addEventListener("scroll", () => {
-    //   this.checkActive()
-    // })
-    window.addEventListener("resize", () => {
-      // this.calcResize()
-      // this.checkActive()
-    })
-  }
-
-  // calcResize() {
-  //   this.setState({
-  //     rect: this.node.getBoundingClientRect(),
-  //     screenWidth: window.innerWidth,
-  //     screenHeight: window.innerHeight,
-  //   })
-  // }
-
-  // checkActive() {
-  //   const rect = this.node.getBoundingClientRect()
-  //   const {name} = this.props.data
-  //   const {active} = this.state
-
-  //   console.log(name, `top: ${rect.top}, bottom: ${rect.bottom}`)
-  //   // console.log(name, "scrollTop: ", document.body.scrollTop)
-
-  //   if ((rect.top >= window.innerHeight || rect.bottom <= 0)) {
-  //     console.log(name, "out!")
-  //     this.setState({active: false})
-  //   } else if ((rect.top < window.innerHeight || rect.bottom > 0)) {
-  //     console.log(name, "in!")
-  //     this.setState({active: true})
-  //   } else {
-  //     // do nothing
-  //   }
-  // }
 
   render () {
     // const {screenWidth, screenHeight} = this.state
     const {data} = this.props
     const {name, slug, price, sold, shopURL, images, description} = data
     const image = _.first(images)
-    const {url: imageURL, width, height} = image
-    const isPortrait = window.innerHeight > window.innerWidth
-    // const initWinWidth = window.innerWidth
-    // const initWinHeight = window.innerHeight
-    // const imageActive = false
-
-    // const widthMultiplier = ((screenWidth - (C.spacing * 2)) / width)
-    // const heightMultiplier = (screenHeight * 0.8) / height
-
-    // const adjustedWidth = isPortrait
-    //   ? width * widthMultiplier
-    //   : width * heightMultiplier
-
-    // const adjustedHeight = isPortrait
-    //   ? height * widthMultiplier
-    //   : height * heightMultiplier
+    const {url: imageURL} = image
 
     const imageEl = (
       <img
@@ -84,19 +17,6 @@ export default class SketchbookUnit extends React.Component {
         height="auto"
       />
     )
-
-    // const imagePlaceholderEl = (
-    //   <div
-    //     style={{
-    //       width: adjustedWidth,
-    //       height: adjustedHeight,
-    //       backgroundColor: "grey"
-    //     }}
-    //   >
-    //   </div>
-    // )
-
-    // const {active} = this.state
 
     return (
       <div
@@ -126,31 +46,3 @@ export default class SketchbookUnit extends React.Component {
     )
   }
 }
-
-
-
-
-/*
-
-        <h3>
-          <p>
-            {images.map((data, index, coll) => {
-              const isLast = data === _.last(coll)
-              return (
-                <span key={index}>
-                  <a href={data.url}>{data.name}</a>{isLast ? null : ", "}
-                </span>
-              )
-            })}
-          </p>
-        </h3>
-
-
-{active ? imageEl : imagePlaceholderEl}
-
-*/
-
-
-
-
-
