1bef8d91d60effe9332ca51167fecb38055729b7
James Paterson <j@presstube.com>
2016-06-30 14:59:10 -0400

    centering on bigger display

--- src/components/SketchbookUnit.js
+++ src/components/SketchbookUnit.js
@@ -6,76 +6,29 @@ export default class SketchbookUnit extends React.Component {
 
   constructor(props) {
     super(props)
-    // window.addEventListener("scroll", () => {
-    //   console.log("scroll")
-    //   this.checkCull()
-    //   // perform cull update
-    // })
-    // window.addEventListener("resize", () => {
-    //   console.log("resize")
-    //   // this.calcRect()
-    //   // store getBoundingClientRect()
-    //   this.checkCull()
-    // })
   }
 
   state = {
     active: true,
-    rect: null
   }
-
-  componentDidMount() {
-    // this.calcRect()
-    // this.checkCull()
-  }
-
-  // calcRect() {
-  //   this.rect = this.node.getBoundingClientRect()
-  // }
-
-  // checkCull() {
-  //   this.rect = this.node.getBoundingClientRect()
-  //   const {top, bottom} = this.rect
-  //   const {innerHeight} = window
-  //   // console.log(this.props.data.name)
-  //   if ((top >= 0 && top <= innerHeight) ||
-  //       (bottom >= 0 && bottom <= innerHeight) ||
-  //       (top < 0 && bottom > innerHeight)) {
-  //     this.setState({active: true})
-  //   } else {
-  //     this.setState({active: false})
-  //   }
-  // }
 
   render () {
-    // const {screenWidth, screenHeight} = this.state
     const {active} = this.state
     const {data} = this.props
     const {name, slug, price, sold, shopURL, images, description} = data
     const image = _.first(images)
-    const {url: imageURL/*, width, height*/} = image
-    // const widthMult = window.innerWidth/width
-    // const dpr = window.devicePixelRatio
+    const {url: imageURL} = image
 
     const imageEl = (
       <div
-        // style={{
-        //   // width: `calc(100% - ${width})`,
-        //   // height: `calc(100% - ${width})`,
-        //   // width: `calc(${width}px * calc(${width}px / 100%)px)`,
-        //   // height: `calc(${height}px * calc(${width}px / 100%)px)`,
-        //   width: (width * widthMult) * dpr,
-        //   height: (height * widthMult) * dpr,
-        //   // height: `calc(${height}px * calc(${width}px / 100%)px)`,
-        //   backgroundColor: "grey",
-        // }}
+        style={{
+        }}
       >
         {active ?
           <img
             src={imageURL}
             width="100%"
             height="auto"
-            // maxHeight="100%"
           /> : null
         }
       </div>
--- src/components/app.js
+++ src/components/app.js
@@ -14,9 +14,7 @@ export default class App extends React.Component {
   }
 
   componentDidMount() {
-    window.addEventListener("resize", () => {
-      this.onResize()
-    })
+    window.addEventListener("resize", this.onResize)
     this.onResize()
   }
 
@@ -28,118 +26,54 @@ export default class App extends React.Component {
     const projectData = this.props.data.projects[0]
     const {name: projectName, units} = projectData
     const {maxWidth} = this.state
-    // const spacing = 40
     return (
-      <div style={{
-        marginTop: C.spacing,
-        // width: "100%",
-        width: "100%",
-        maxWidth,
-        // width: `calc(100% - ${C.spacing}px)`,
-        // textAlign: "center", // doesn't really work, why?
-      }}>
-        <div // nav
-          style={{
-            paddingLeft: C.spacing / 2,
-            paddingRight: C.spacing / 2,
-            position: "relative",
-            width: 80,
-            height: 80,
-            marginBottom: C.spacing,
-            // borderStyle: "solid",
-            // borderWidth: 1,
-            // borderRadius: 4,
-            // overflow: "hidden",
-          }}
-        >
-          <TopScrolly name="brand"/>
+      <div
+        style={{
+          position: "relative",
+          width: "100%",
+          textAlign: "center",
+        }}
+      >
+        <div style={{
+          display: "inline-block",
+          position: "relative",
+          marginTop: C.spacing,
+          width: "100%",
+          // maxWidth,
+          maxWidth: 900,
+          textAlign: "left",
+        }}>
+          <div // nav
+            style={{
+              paddingLeft: C.spacing / 2,
+              paddingRight: C.spacing / 2,
+              position: "relative",
+              width: 80,
+              height: 80,
+              marginBottom: C.spacing,
+            }}
+          >
+            <TopScrolly name="brand"/>
+          </div>
+          <h1
+            style={{
+              paddingLeft: C.spacing / 2,
+              paddingRight: C.spacing / 2,
+              marginBottom: C.spacing,
+            }}
+          >
+            {projectName}
+          </h1>
+          {units.map((data, index) => {
+            return (
+              <SketchbookUnit
+                key={index}
+                data={data}
+              />
+            )
+          })}
         </div>
-        <h1
-          style={{
-            paddingLeft: C.spacing / 2,
-            paddingRight: C.spacing / 2,
-            marginBottom: C.spacing,
-          }}
-        >
-          {projectName}
-        </h1>
-        {units.map((data, index) => {
-          return (
-            <SketchbookUnit
-              key={index}
-              data={data}
-            />
-          )
-        })}
       </div>
     )
   }
 }
-
-/*
-        <div /* project videos
-          style={{
-            marginBottom: spacing
-          }}>
-          <iframe
-            style={{
-              // maxWidth: 500
-            }}
-            src="https://player.vimeo.com/video/172081140"
-            width="100%"
-            height="100%"
-            frameborder="0"
-            webkitallowfullscreen
-            mozallowfullscreen
-            allowfullscreen
-          ></iframe>
-          <h3>
-            <p>
-              <a href="">Intro</a>,
-              <a href="">1-Life</a>,
-              <a href="">2-Imagination</a>,
-              <a href="">3-Meditation</a>,
-              <a href="">4-Systems</a>
-            </p>
-          </h3>
-        </div>
-
-
-
-
-
-        <div // project unit
-          style={{
-          }}
-        >
-          <img
-            src="img/dark-lord-grocery-delivery.jpg"
-            style={{width: "100%"}}
-          />
-          <h2>Dark Lord Grocery Delivery</h2>
-          <h3>
-            <p>
-              <a href="">full</a>,
-              <a href="">detail 1</a>,
-              <a href="">detail 2</a>,
-              <a href="">detail 3</a>
-            </p>
-          </h3>
-          <p>An 8x10.6" original sketchbook drawing from the Psychic Vomit Portrait series.</p>
-          <p>Not all dark lords have glamorous jobs like stealing souls and ruling galaxies.</p>
-          <p>Some deliver dark groceries.</p>
-          <h3>
-            <a
-              href="https://www.etsy.com/listing/400376213/dark-lord-grocery-delivery?ref=shop_home_active_1"
-            >
-              SOLD
-            </a>
-          </h3>
-        </div>
-
-
-
-
-
-
-*/
